b4acf90bfa9ceccdc8c2f9b393bfb7e152122c4e
Mahalakshmi03935 <162538876+Mahalakshmi03935@users.noreply.github.com>
2024-04-08 10:25:38 +0530

    Created a dataframe and performed various queries

--- src/assignment_2.py/util.py
+++ src/assignment_2.py/util.py
@@ -1,1 +1,51 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql.functions import udf
+
+def create_spark_session(app_name):
+    """
+    Create SparkSession
+    """
+    return SparkSession.builder.appName(app_name).getOrCreate()
+
+def define_schema():
+    """
+    Define schema for credit card DataFrame
+    """
+    credit_card_df_schema = StructType([
+        StructField("card_number", StringType())
+    ])
+    return credit_card_df_schema
+
+def read_dataset(spark, path, header=True, infer_schema=True):
+    """
+    Read dataset into a DataFrame
+    """
+    return spark.read.csv(path, header=header, inferSchema=infer_schema)
+
+def increase_partition_size(df, num_partitions):
+    """
+    Increase the partition size of DataFrame
+    """
+    return df.repartition(num_partitions)
+
+def decrease_partition_size(df, num_partitions):
+    """
+    Decrease the partition size of DataFrame
+    """
+    return df.coalesce(num_partitions)
+
+def mask_card_number(card_number):
+    """
+    Mask card numbers
+    """
+    masked_character = len(card_number) - 4
+    masked_number = ('*' * masked_character) + card_number[-4:]
+    return masked_number
+
+def register_udf(spark):
+    """
+    Register UDF for masking card numbers
+    """
+    return udf(mask_card_number, StringType())
 
